63e26ec90ae04a57fb25aba4907b8cdcf8795243
Your Name <ABDULSELAM ELAHMED>
2024-10-12 16:51:05 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace secindTry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(301, -10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 77);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADAM ASMA";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.Snow;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(16, 254);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 123);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Snow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "CHECK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(134, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(378, 77);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "---------------";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(672, 396);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Snow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(872, 447);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(12, 10);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "New";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.IndianRed;
+            this.label4.Location = new System.Drawing.Point(411, 425);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 32);
+            this.label4.TabIndex = 0;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.IndianRed;
+            this.label5.Location = new System.Drawing.Point(1, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 32);
+            this.label5.TabIndex = 6;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.IndianRed;
+            this.label6.Location = new System.Drawing.Point(1, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 32);
+            this.label6.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.IndianRed;
+            this.label7.Location = new System.Drawing.Point(380, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 27);
+            this.label7.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label8.Location = new System.Drawing.Point(794, 84);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 19);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "made by selam";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "adam asma";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label8;
+
+        private System.Windows.Forms.Label label7;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label4;
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace secindTry
+{
+    public partial class Form1 : Form
+    {
+       static string[] tr =
+        { "adana", "adiyaman", "afyonkarahisar", "agri", "aksaray", "amasya", "ankara",
+            "antalya", "ardahan", "artvin", "aydin", "balikesir", "bartin", "batman",
+            "bayburt", "bilecik", "bingol", "bitlis", "bolu", "burdur", "bursa",
+            "canakkale", "cankiri", "corum", "denizli", "diyarbakir", "duzce",
+            "edirne", "elazig", "erzincan", "erzurum", "eskisehir", "gaziantep",
+            "giresun", "gumushane", "hakkari", "hatay", "igdir", "isparta", "istanbul",
+            "izmir", "kahramanmaras", "karabuk", "karaman", "kars", "kastamonu",
+            "kayseri", "kirikkale", "kirklareli", "kirsehir", "kilis", "kocaeli",
+            "konya", "kutahya", "malatya", "manisa", "mardin", "mersin", "mugla",
+            "mus", "nevsehir", "nigde", "ordu", "osmaniye", "rize", "sakarya", "samsun",
+            "sanliurfa", "siirt", "sinop", "sivas", "sirnak", "tekirdag", "tokat",
+            "trabzon", "tunceli", "usak", "van", "yalova", "yozgat", "zonguldak"};
+       
+        static  string string_word;                                      //the word
+        static int rnd;                                                  //random number
+        static int scoreP_1 = 0;
+        static int scoreP_2 = 0;
+        static bool turn = true;                                         //true bool is for 1 player false bool is for 2 player . will start with 1 player
+        static  string secret_string_word;                               //the word as secre word
+        static  char[] secret_word;                                      //the secret word as char array 
+        static char letter;                                              // the letter that we got from user
+        static int HEALTH = 7;          
+        
+        Random random = new Random(); 
+        public Form1()
+        { this.BackgroundImage= Image.FromFile("frames\\hangman_1.png");                 //set default image 
+            InitializeComponent();
+            rnd = random.Next(0, tr.Length);                             //make random number from zero to the size of the words array
+            string_word = tr[rnd];                                       // create the word
+            secret_string_word = new string('-',string_word.Length);     //create the secret word 
+           secret_word = secret_string_word.ToCharArray();               // convert the secret word to char array
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {   label5.Text="1P Score : "+scoreP_1;
+            label6.Text="2P Score : "+scoreP_2;
+            if (turn == true)
+                label7.Text = "1. Player turn";
+            else
+                label7.Text = "2. Player turn";
+            label3.Text = "Health : " + HEALTH.ToString()+"X ❤️‍";       //print the health
+            label2.Text = secret_string_word;                            //show the secret word on screen
+        }
+        private void button1_Click(object sender, EventArgs e)           //when user press Check button this will work
+        {
+            if (textBox1.Text == "")                                     //control if the input is empty
+            {
+               MessageBox.Show("Please enter a letter");
+            }
+            else                                                         //if the input is not empty the variable letter will be the letter that we got from input
+            {  
+                letter =textBox1.Text[0];
+            }
+            textBox1.Clear();                                            // clear the textbox
+            for (int j = 0; j < string_word.Length; j++)                 //for loop to control every index 
+            {
+                if (letter == string_word[j])                            //if the letter exist will do changes
+                {
+                    label4.Text="";                                      //remove not exist lable
+                    secret_word[j] = letter;                             //put the letter that user enter in the secret word
+                    CharArrayToString(secret_word, secret_string_word);                 //function to convert char array to string
+                    secret_string_word = new string(secret_word);        //convert the secret word array to string    
+                    label2.Text = secret_string_word;                    // cause the label doesnt accept char arrays
+                }
+            }
+            if (!string_word.Contains(letter))                           //if the secret word doesnt find
+            {
+                    HEALTH--;                                            //the health will get damage
+                    switch(HEALTH)                                       //when the health get down the background will change to different photos
+                    {
+                        case 6:
+                            this.BackgroundImage= Image.FromFile("frames\\hangman0.png");
+                            break;
+                        case 5:
+                            this.BackgroundImage= Image.FromFile("frames\\hangman1.png");
+                            break;
+                        case 4:
+                            this.BackgroundImage= Image.FromFile("frames\\hangman2.png");
+                            break;
+                        case 3:
+                            this.BackgroundImage= Image.FromFile("frames\\hangman3.png");
+                            break;
+                        case 2:       
+                            this.BackgroundImage= Image.FromFile("frames\\hangman4.png");
+                            break;
+                        case 1:
+                            this.BackgroundImage= Image.FromFile("frames\\hangman5.png");
+                            break;
+                        case 0:
+                            this.BackgroundImage= Image.FromFile("frames\\hangman6.png");
+                            break;
+                    }
+                    label3.Text = "Health : " + HEALTH.ToString()+"X \u2764\ufe0f"; //update the Health on screen
+                    
+                    label4.Text="not exist";                             //print this cause the letter not exist
+
+                    if (HEALTH == 0)                                     //when the Health over
+                    {
+                        MessageBox.Show("You lose , the word was : " +
+                         string_word.ToUpper()+"\nthe other player turn!"); //messagebox to show what the word was
+                        rnd = random.Next(0, tr.Length);                 //update the random number
+                        label4.Text = "";                                //remove the not exist word
+                        string_word = tr[rnd];                           //set new word
+                        secret_string_word =
+                            new string('-', string_word.Length);       //make the secret word from the word length
+                        secret_word = secret_string_word.ToCharArray();  //convert the word to char array
+                        label2.Text = secret_string_word;                //show the secret word on the screen
+                        this.BackgroundImage = Image.FromFile("frames\\hangman_1.png");  //set the default background
+                        HEALTH = 7;                                      //set new Health
+                        label3.Text =
+                            "Health : " + HEALTH.ToString() + "X \u2764\ufe0f"; //show the new Health on screen
+                        if (turn == false)                                //if the turn was in 2p
+                        { 
+                            turn = true;                                  //the turn will be in 1p
+                            label7.Text = "1. Player turn";               //show on the screen
+                        }
+                        else{                                            //if the turn was in 1p
+                            turn = false;                                //the turn will be in 2p
+                            label7.Text = "2. Player turn";              //show on the screen 
+                        } 
+                    }
+            }
+            if (!secret_word.Contains('-'))                              //if the word get finished
+            {
+                MessageBox.Show("GREAT JOB \ud83d\udc4f"+"\n+1 Point"); //congratilation in messagebox
+                rnd = random.Next(0, tr.Length);
+                label4.Text="";
+                string_word = tr[rnd];
+                secret_string_word = new string('-',string_word.Length);
+                secret_word = secret_string_word.ToCharArray();
+                label2.Text = secret_string_word;
+                label1.Text = "ADAM ASMA";
+                if (turn == true)
+                {
+                    scoreP_1++;
+                    label5.Text = "1P Score : " + scoreP_1.ToString();
+                }
+                else
+                {
+                    scoreP_2++;
+                    label6.Text = "2P Score : " + scoreP_2.ToString();
+                }
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e){}
+        private void label2_Click(object sender, EventArgs e) {label1.Text = "ASMA ADAM"; }
+        private void label3_Click(object sender, EventArgs e)            //here will show the secret word when Health get pressed
+        {label1.Text = string_word; }                                    //to disapper it press on the secret word
+        public static string CharArrayToString(char[] letter, string str) //function that convert the char array to string
+        { StringBuilder sb = new StringBuilder(str);
+            for (int j = 0; j < letter.Length; j++)
+            { sb.Append(letter[j]); }
+            return sb.ToString(); }
+
+        private void button2_Click(object sender, EventArgs e){ }
+        private void label4_Click(object sender, EventArgs e){ }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label5_Click(object sender, EventArgs e)            // secret trick to set new word and recharg the Health
+        { rnd = random.Next(0, tr.Length);
+            label4.Text = "";
+            string_word = tr[rnd];
+            secret_string_word = new string('-', string_word.Length);
+            secret_word = secret_string_word.ToCharArray();
+            label2.Text = secret_string_word;
+            this.BackgroundImage = Image.FromFile("frames\\hangman_1.png");
+            HEALTH = 7;
+            label3.Text = "Health : " + HEALTH.ToString()+"X \u2764\ufe0f";
+            label1.Text = "ADAM ASMA";
+        }
+    }
+}
